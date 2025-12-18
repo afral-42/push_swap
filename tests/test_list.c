703d55b1cc85e62ba908fac283ef04fd1af144f8
Anselme Bounouar <abounoua@z3r12p5.42lyon.fr>
2025-12-18 14:46:13 +0100

    Added unit tests for lstcheck_duplicate

--- tests/test_list.c
+++ tests/test_list.c
@@ -40,8 +40,72 @@ void	test_lstadd_front(void)
 	free_lst(lst);
 }
 
+void	test_lstcheck_duplicate(void)
+{
+	t_list	*lst;
+	t_list	*lst_b;
+	t_list	*lst_c;
+	t_list	*lst_d;
+
+	lst = lstnew(1);
+	lst_b = lstnew(2);
+	lst_c = lstnew(3);
+	lst_d = lstnew(10);
+
+	lstadd_front(&lst, lstnew(2));
+	lstadd_front(&lst, lstnew(3));
+	lstadd_front(&lst, lstnew(4));
+	lstadd_front(&lst, lstnew(5));
+	lstadd_front(&lst, lstnew(6));
+	lstadd_front(&lst, lstnew(7));
+	lstadd_front(&lst, lstnew(8));
+
+	lstadd_front(&lst_b, lstnew(10));
+	lstadd_front(&lst_b, lstnew(3));
+	lstadd_front(&lst_b, lstnew(-6));
+	lstadd_front(&lst_b, lstnew(1));
+	lstadd_front(&lst_b, lstnew(2));
+	lstadd_front(&lst_b, lstnew(7));
+	lstadd_front(&lst_b, lstnew(4));
+
+	lstadd_front(&lst_c, lstnew(4));
+	lstadd_front(&lst_c, lstnew(-25454));
+	lstadd_front(&lst_c, lstnew(1485512));
+	lstadd_front(&lst_c, lstnew(951));
+	lstadd_front(&lst_c, lstnew(-92122));
+	lstadd_front(&lst_c, lstnew(0));
+	lstadd_front(&lst_c, lstnew(4));
+	lstadd_front(&lst_c, lstnew(5225));
+	lstadd_front(&lst_c, lstnew(91));
+	lstadd_front(&lst_c, lstnew(78));
+	lstadd_front(&lst_c, lstnew(20));
+	lstadd_front(&lst_c, lstnew(15200));
+	lstadd_front(&lst_c, lstnew(321));
+
+
+	lstadd_front(&lst_d, lstnew(9));
+	lstadd_front(&lst_d, lstnew(7));
+	lstadd_front(&lst_d, lstnew(6));
+	lstadd_front(&lst_d, lstnew(5));
+	lstadd_front(&lst_d, lstnew(4));
+	lstadd_front(&lst_d, lstnew(3));
+	lstadd_front(&lst_d, lstnew(2));
+	lstadd_front(&lst_d, lstnew(1));
+
+	assert(lstcheck_duplicate(lst) == 0);
+	assert(lstcheck_duplicate(lst_b) == 1);
+	assert(lstcheck_duplicate(lst_c) == 1);
+	assert(lstcheck_duplicate(lst_d) == 0);
+
+	free_lst(lst);
+	free_lst(lst_b);
+	free_lst(lst_c);
+	free_lst(lst_d);
+}
+
 int	main(void)
 {
 	test_lstnew();
 	test_lstadd_front();
+	test_lstcheck_duplicate();
 }
