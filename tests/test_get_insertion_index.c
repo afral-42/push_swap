7b233628d7ed7c4d7609099527aef40e5fbc113a
Antoine Rebillard <arebilla@z4r3p7.42lyon.fr>
2026-01-09 11:05:07 +0100

    add test

--- tests/test_get_insertion_index.c
+++ tests/test_get_insertion_index.c
@@ -7,6 +7,7 @@ int	main(void)
 {
 	int	numbers[] = { 6, 8, 2, 4 };
 	int	numbers2[] = { 2, 4, 6, 8 };
+	int numbers3[] = { 2, 6, 10, 4, 8 };
 	t_stack	*s;
 
 	s = generate_custom_stack(numbers, 4);
@@ -23,5 +24,8 @@ int	main(void)
 	assert(get_insertion_index(s, 5, 0, 4) == 2);
 	assert(get_insertion_index(s, 7, 0, 4) == 3);
 	assert(get_insertion_index(s, 9, 0, 4) == 0);
+	free_stack(s);
+	s = generate_custom_stack(numbers3, 4);
+	assert(get_insertion_index(s, 7, 0, 2) == 2);
 	free_stack(s);
 }
